26e93c106269c1d1544055e601ca3392dc7d7b6a
kellyfreitas <kellyfreitas418@gmail.com>
2021-10-02 20:18:08 -0300

    pidm_Atividade 01

---
+++ Atividades/Atividade1/components/Aluna.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+
+function Aluna() {
+    return (
+        <View>
+            <View ></View>
+            <Text style={{ fontSize: '22px',fontWeight: 'bold' }}>
+                Rayane Kelly de Sousa Freitas
+            </Text>
+
+            <Text style={{ fontWeight: '500' }}>
+                22 anos, Design Digital<br/>
+                Capistrano 
+            </Text>
+        </View>
+    )
+}
+
+export default Aluna;
